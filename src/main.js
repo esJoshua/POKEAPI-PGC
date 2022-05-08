057e10379a9026e74e156319cbb1e0cabc52d833
Joshua Espinoza <josh6669@gmail.com>
2022-05-08 19:27:26 -0400

    primevue primeflex primeicons included

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css";
+import "../node_modules/primeflex/primeflex.css";
+
+const app = createApp(App);
+app.use(store).use(router).use(PrimeVue);
+
+
+app.mount("#app");
