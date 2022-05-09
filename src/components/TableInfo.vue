efec5e8c4e58361254a3d8df3fb382c84ff91cfa
Joshua Espinoza <josh6669@gmail.com>
2022-05-09 12:50:57 -0400

    ready to deploy with firebase

--- src/components/TableInfo.vue
+++ src/components/TableInfo.vue
@@ -1,52 +1,45 @@
 <template>
-  <div>
-    <DataTable
-      :value="currentPokemon"
-      responsiveLayout="scroll"
-      :paginator="true"
-      :rows="5"
-      paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-      :rowsPerPageOptions="[5, 15, 30]"
-      currentPageReportTemplate="Mostrando registros del {first} al {last} de un total de {totalRecords} registros."
-      class="mx-6 mt-8 bg-white border-900"
-      scrollHeight="520px"
-      :scrollable="true"
-    >
-      <template #empty>
-        <div class="text-900">{{ msgEmptyTable }}</div>
+  <DataTable
+    :value="currentPokemon"
+    responsiveLayout="scroll"
+    :paginator="true"
+    :rows="5"
+    paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+    :rowsPerPageOptions="[5, 15, 30]"
+    currentPageReportTemplate="Mostrando registros del {first} al {last} de un total de {totalRecords} registros."
+    class="mx-6 mt-8 bg-white border-900"
+    scrollHeight="520px"
+    :scrollable="true"
+  >
+    <template #empty>
+      <div class="text-900">{{ msgEmptyTable }}</div>
+    </template>
+    <Column field="favorite" header="Favorito" class="border-900">
+      <template #body="slotProps">
+        <Button
+          class="bg-cyan-500 button--radius"
+          @click="addFavorite(slotProps.data)"
+        >
+          AGREGAR
+        </Button>
       </template>
-      <Column field="favorite" header="Favorito" class="border-900">
-        <template #body="slotProps">
-          <Button
-            class="bg-cyan-500 button--radius"
-            @click="addFavorite(slotProps.data)"
-          >
-            AGREGAR
-          </Button>
-        </template>
-      </Column>
-      <Column field="name" header="Nombre" class="border-900 text-900">
-      </Column>
-      <Column field="height" header="Alto" class="border-900 text-900"></Column>
-      <Column
-        field="weight"
-        header="Ancho"
-        class="border-900 text-900"
-      ></Column>
-      <Column field="image" header="Imagen" class="border-900">
-        <template #body="slotProps">
-          <img :src="slotProps.data.image" class="h-5rem w-5rem" /> </template
-      ></Column>
-      <Column field="delete" header="Eliminar" class="border-900">
-        <template #body="slotProps">
-          <Button
-            class="border-circle p-button-danger"
-            @click="removePokemon(slotProps.data)"
-            ><i class="pi pi-trash" style="font-size: 25px"></i
-          ></Button> </template
-      ></Column>
-    </DataTable>
-  </div>
+    </Column>
+    <Column field="name" header="Nombre" class="border-900 text-900"> </Column>
+    <Column field="height" header="Alto" class="border-900 text-900"></Column>
+    <Column field="weight" header="Ancho" class="border-900 text-900"></Column>
+    <Column field="image" header="Imagen" class="border-900">
+      <template #body="slotProps">
+        <img :src="slotProps.data.image" class="h-5rem w-5rem" /> </template
+    ></Column>
+    <Column field="delete" header="Eliminar" class="border-900">
+      <template #body="slotProps">
+        <Button
+          class="border-circle p-button-danger"
+          @click="removePokemon(slotProps.data)"
+          ><i class="pi pi-trash" style="font-size: 25px"></i
+        ></Button> </template
+    ></Column>
+  </DataTable>
 </template>
 
 <script>
